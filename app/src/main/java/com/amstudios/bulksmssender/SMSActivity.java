af162d6aa7bbd7f91f93ee77a7d5db0e59f248b2
Syed Asad Murtaza <asadmurtaza110@gmail.com>
2016-01-27 21:12:05 +0500

    Layout configured and methods refactoring done.

--- app/src/main/java/com/amstudios/bulksmssender/SMSActivity.java
+++ app/src/main/java/com/amstudios/bulksmssender/SMSActivity.java
@@ -5,28 +5,101 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SMSActivity extends AppCompatActivity {
+
+
+    private Button btnSend;
+    private EditText etText,etNumber,etNumberSMS;
+    int a,c,d,e;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sms);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+
+        etText = (EditText) findViewById(R.id.etext);
+        etNumber = (EditText) findViewById(R.id.enumber);
+        etNumberSMS = (EditText) findViewById(R.id.smsno);
+
+        btnSend = (Button) findViewById(R.id.btnSend);
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+        generateToast();
+
+        assignValue();
+
+        if (c == 4 && d == 4 && e == 4) {
+            String text = etText.getText().toString();
+            String number = etNumber.getText().toString();
+            String nos = etNumberSMS.getText().toString();
+
+            int no = Integer.parseInt(nos);
+
+
+            SmsManager sms = SmsManager.getDefault();
+
+
+            for (int i = 0; i < no; i++) {
+                sms.sendTextMessage(number, null, text, null, null);
+            }
+
+            Toast.makeText(getApplicationContext(), "Messages Sent!", Toast.LENGTH_LONG).show();
+        }
     }
+
+});
+
+
+
+
+        }
+
+
+    private boolean isEmpty(EditText text)
+
+    {
+
+        if(text.getText().toString().trim().length()>0)
+            return false;//Something is written in text fields
+        else
+        return true;//Field is Empty
+
+    }
+
+    private void generateToast(){
+
+        if(isEmpty(etText)==true)
+            Toast.makeText(getApplicationContext(), "Enter some text", Toast.LENGTH_SHORT).show();
+        if(isEmpty(etNumber)==true)
+            Toast.makeText(getApplicationContext(), "Enter number", Toast.LENGTH_SHORT).show();
+        if(isEmpty(etNumberSMS)==true)
+            Toast.makeText(getApplicationContext(), "Enter number of SMS to send", Toast.LENGTH_SHORT).show();
+        a=2;
+    }
+
+    private void assignValue(){
+
+
+        if(isEmpty(etText)==false)
+            c=4;
+         if(isEmpty(etNumber)==false)
+            d=4;
+        if(isEmpty(etNumberSMS)==false)
+            e=4;
+
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,6 +107,7 @@ public class SMSActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_sms, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
